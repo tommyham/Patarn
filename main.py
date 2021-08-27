af80307c83eb473afdf49926f7c1cfe9a5c6a858
tommyham <tob19098.kez052080uns@gmail.com>
2021-08-27 13:20:25 +0900

    Creat main code

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import math
+#import seaborn as sns
+import os
+from Class import Calculation,Feature,Data_Set
+from Read import Read
+from Draw import Axis,Draw
+from Variable import Variable
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# import scipy.stats
+# from sklearn import preprocessing
+import glob
